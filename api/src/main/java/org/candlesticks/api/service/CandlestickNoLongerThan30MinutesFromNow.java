860e92a4b97679cdf4af581f9c3f174b4c185528
slyncon <io.lynconr@gmail.com>
2023-02-12 18:34:03 -0500

    code cleanup

--- api/src/main/java/org/candlesticks/api/service/CandlestickNoLongerThan30MinutesFromNow.java
+++ api/src/main/java/org/candlesticks/api/service/CandlestickNoLongerThan30MinutesFromNow.java
@@ -11,16 +11,13 @@ public class CandlestickNoLongerThan30MinutesFromNow {
 
     protected CandlestickNoLongerThan30MinutesFromNow(){}
 
-//      https://stackoverflow.com/questions/52623282/how-to-get-30-min-back-instance-object-instance-now-30-min
     public static List<Quote> validate(List<Quote> quotes) {
 
         List<Quote> quotesForThePast30Minutes = new ArrayList<>();
 
-        Instant now = Instant.now();
-
         quotes.forEach(element -> {
 
-            Instant thirtyMinutesBeforeNow = now.minus(30, ChronoUnit.MINUTES);
+            Instant thirtyMinutesBeforeNow = Instant.now().minus(30, ChronoUnit.MINUTES);
 
             if (! element.getTimestamp().isBefore(thirtyMinutesBeforeNow)) {
                 quotesForThePast30Minutes.add(element);
